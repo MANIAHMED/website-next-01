50d9f0e0c077d22d835814ab5529855406ab76df
MAniAhmed <moizubit123@gmail.com>
2021-03-21 03:07:28 -0700

    Navbar completed

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Contact() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title> Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar/>
+      <h1>Contact</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title> Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
-    </div>
-  )
+      <Navbar />
+    </>
+  );
 }
